27cbf09f708fa8aeeced10926af3f0d02b28c050
alexhamidi <147952065+alexhamidi@users.noreply.github.com>
2024-08-12 10:50:36 -0700

    updated cors

--- TritonTool-Backend/server.js
+++ TritonTool-Backend/server.js
@@ -18,7 +18,7 @@ require('dotenv').config();
 app.use(cookieParser());
 app.use(express.json()); 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_ANGULAR], 
+    origin: [process.env.FRONTEND_URL], 
     methods: ['POST', 'GET', 'DELETE'], 
     credentials: true 
 }));
